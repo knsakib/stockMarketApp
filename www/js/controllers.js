954eb49912fe514fa1eb24bb839df5df3cd9da87
khalidsakib <knsakib@gmail.com>
2016-07-16 17:59:47 -0400

    error loading json object

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -61,8 +61,23 @@ $scope.myStocksArray=[
 
 }])
 
-.controller('StockCtrl', ['$scope', '$stateParams',
-  function($scope, $stateParams) {
+.controller('StockCtrl', ['$scope', '$stateParams', 'stockDataService',
+  function($scope, $stateParams, stockDataService) {
+
     $scope.ticker = $stateParams.stockTicker;
+
+    $scope.$on("$ionicView.afterEnter", function(){
+      getPriceData();
+    });
+
+    function getPriceData() {
+
+      var promise = stockDataService.getPriceData($scope.ticker);
+      promise.then(function(data){
+        console.log(data);
+      });
+
+    }
+
 
 }]);
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,30 @@
 angular.module('stockMarketApp.services',[])
 
+.factory('stockDataService', function($q, $http) {
+
+  var getPriceData = function(ticker) {
+
+  var deferred = $q.defer(),
+  url = "https://feeds.finance.yahoo.com/rss/2.0/headline?s=" + ticker + "&region=US&lang=en-US";
+
+  $http.get(url)
+  .success(function(json) {
+    var jsonData=json.list.resources[0].resource.fields;
+    //console.log(jsonData.data.list.resources[0].resource.fields);
+    deferred.resolve(jsonData);
+  })
+  .error(function(error){
+    console.log("Price data error:" + error);
+    deferred.reject();
+
+  });
+  return deferred.promise;
+
+};
+  return {
+    getPriceData: getPriceData
+  };
+
+})
 
 ;
